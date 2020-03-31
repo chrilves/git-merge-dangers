d0f682af66b2b388e3c46f8463d1479f8ab59216
Christophe Calvès <christophe.calves@gmail.com>
2020-03-31 12:06:34 +0200

    Bob's Work: Log time spent by function f
    This features log the time spend computing
    the sum in function f.

--- test.c
+++ test.c
@@ -1,15 +1,25 @@
 #include "stdio.h"
 #include <assert.h>
+#include <time.h>
+#include <string.h>
 
 long f() {
   long iterations = 10;
+  struct timespec tspec = {0,0};
+  long nanosecs;
   long accumulator = 0;
+
+  clock_gettime(CLOCK_MONOTONIC, &tspec);
+  memcpy(&nanosecs, &(tspec.tv_nsec), sizeof(long));
 
   while (iterations > 0) {
     accumulator = iterations + accumulator;
     iterations--;
   }
     
+  clock_gettime(CLOCK_MONOTONIC, &tspec);
+  printf("f took about %d nano-seconds\n", tspec.tv_nsec - nanosecs);
+
   return accumulator;
 }
 
